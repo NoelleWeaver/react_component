3efce7478c043933ddc9791e63287e79adf732be
NoelleWeaver <141285591+NoelleWeaver@users.noreply.github.com>
2024-11-26 13:13:18 -0700

    fixed a few things

--- src/components/gallery/GalleryComponent.jsx
+++ src/components/gallery/GalleryComponent.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import './Gallery.css';
 
-export default function GalleryComponent({profileImage}) {
+export default function GalleryComponent({pic1, pic2, pic3, pic4, pic5, pic6, pic7, pic8, pic9, pic10, pic11, pic12, pic13, pic14, pic15, pic16, pic17, pic18, pic19, pic20, pic21, pic22, pic23, pic24, pic25, pic26, pic27, pic28, pic29, pic30, pic31, pic32}) {
   
     return (
       <>
@@ -10,7 +10,38 @@ export default function GalleryComponent({profileImage}) {
       
         <div className="gallery-container">
           <div className="gallery-box">
-            <img className="image" src={profileImage} />
+            <img className="image" src={pic1} />
+            <img className="image" src={pic2} />
+            <img className="image" src={pic3} />
+            <img className="image" src={pic4} />
+            <img className="image" src={pic5} />
+            <img className="image" src={pic6} />
+            <img className="image" src={pic7} />
+            <img className="image" src={pic8} />
+            <img className="image" src={pic9} />
+            <img className="image" src={pic10} />
+            <img className="image" src={pic11} />
+            <img className="image" src={pic12} />
+            <img className="image" src={pic13} />
+            <img className="image" src={pic14} />
+            <img className="image" src={pic15} />
+            <img className="image" src={pic16} />
+            <img className="image" src={pic17} />
+            <img className="image" src={pic18} />
+            <img className="image" src={pic19} />
+            <img className="image" src={pic20} />
+            <img className="image" src={pic21} />
+            <img className="image" src={pic22} />
+            <img className="image" src={pic23} />
+            <img className="image" src={pic24} />
+            <img className="image" src={pic25} />
+            <img className="image" src={pic26} />
+            <img className="image" src={pic27} />
+            <img className="image" src={pic28} />
+            <img className="image" src={pic29} />
+            <img className="image" src={pic30} />
+            <img className="image" src={pic31} />
+            <img className="image" src={pic32} />
           </div>
         </div>
       </body>
